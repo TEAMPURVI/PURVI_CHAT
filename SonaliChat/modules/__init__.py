e7341c6719cc57139b3fe02d433723c9bb020ef0
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2026-08-14 05:21:58 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

---
+++ SonaliChat/modules/__init__.py
@@ -0,0 +1,27 @@
+# =======================================================
+# ©️ 2026-27 All Rights Reserved by Purvi Bots (TEAMPURVI) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @TheSigmaCoder
+# =======================================================
+
+from pathlib import Path
+
+def list_modules():
+    mod_dir = Path(__file__).parent
+    return [
+        file.stem
+        for file in mod_dir.glob("*.py")
+        if file.is_file() and file.name != "__init__.py"
+    ]
+
+ALL_MODULES = frozenset(sorted(list_modules()))
+
+# ======================================================
+# ©️ 2026-27 All Rights Reserved by Purvi Bots (TEAMPURVI) 😎
+
+# 🧑‍💻 Developer : t.me/TheSigmaCoder
+# 🔗 Source link : GitHub.com/TEAMPURVI/PURVI_CHAT
+# 📢 Telegram channel : t.me/Purvi_Bots
+# =======================================================
